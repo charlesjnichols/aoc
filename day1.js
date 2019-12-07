a9cc839af061cc47f38830bed751ebd62e6d03e9
CJ <ccdtrd@gmail.com>
2019-12-07 16:55:37 +0000

    feat: day1 part2

--- day1.js
+++ day1.js
@@ -1,8 +1,19 @@
 const fs = require('fs');
+const _ = require('lodash');
 
-const answer = fs.readFileSync('day1.txt')
-                    .toString('utf8')
-                    .split(/\r?\n/)
-                    .reduce((acc, x) => acc + Math.floor(x / 3) - 2, 0);
+const _fuelRequired = x => Math.floor(x / 3) - 2;
+const _totalFuelRequired = x => {
+    const required = _fuelRequired(x);
+    return required <= 0 ? 0 : required + _totalFuelRequired(required);
+}
 
-console.log(answer);
+const inputs = fs.readFileSync('day1.txt')
+    .toString('utf8')
+    .split(/\r?\n/)
+    .map(x => Number.parseInt(x));
+
+const part1 = inputs.reduce((acc, x) => acc + _fuelRequired(x), 0);
+console.log(part1);
+
+const part2 = inputs.reduce((acc, x) => acc + _totalFuelRequired(x), 0);
+console.log(part2);
