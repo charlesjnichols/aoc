398d3035b6a4a53ddbb9ae2268e32950d06bed39
cjnicho <cjnicho@uwe.nsa.gov>
2024-12-02 18:47:51 +0000

    new year

---
+++ day1.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+
+	"github.com/samber/lo"
+)
+
+func main() {
+
+	contents, err := os.ReadFile("day1-page.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var first, second []int
+	for _, s := range strings.Split(strings.TrimSpace(string(contents)), "\n") {
+		words := strings.Fields(s)
+
+		left, _ := strconv.Atoi(words[0])
+		right, _ := strconv.Atoi(words[1])
+
+		first, second = append(first, left), append(second, right)
+	}
+
+	sort.Ints(first)
+	sort.Ints(second)
+
+	distance, similarity := 0, 0
+
+	for _, i := range lo.Range(len(first)) {
+		v := first[i] - second[i]
+
+		src := first[i]
+		count := lo.CountBy(second, func(comp int) bool {
+			return comp == src
+		})
+		similarity += src * count
+		distance += max(v, -v)
+	}
+	fmt.Println(distance, similarity)
+
+}
